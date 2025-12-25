d00f788a34b0a3de07aff8871b80e355753c48a4
Vinaya-16 <vinayapatole16@gmail.com>
2025-12-25 14:22:10 +0530

    Create Load cust_az12.sql

---
+++ scripts/silver/Load cust_az12.sql
@@ -0,0 +1,29 @@
+/*
+==============================================================================
+CLEAN AND LOAD THE DATA INTO SILVER.ERP_CUST_AZ12
+==============================================================================
+*/
+
+-- TRUNCATING TABLLE
+TRUNCATE TABLE silver.erp_cust_az12;
+
+PRINT('Inserting Data Into Table silver.erp_cust_az12');
+
+-- inserting clean data into table silver.erp_cust_az12
+
+INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
+SELECT
+CASE 
+	WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+	ELSE cid
+END AS cid,
+CASE 
+	WHEN bdate > GETDATE() THEN NULL
+	ELSE bdate
+END AS bdate,
+CASE
+	WHEN UPPER(TRIM(gen)) IN ('F', 'Female') THEN 'Female'
+	WHEN UPPER(TRIM(gen)) IN ('M', 'Male') THEN 'Male'
+	ELSE 'n/a'
+END AS gen
+FROM bronze.erp_cust_az12
