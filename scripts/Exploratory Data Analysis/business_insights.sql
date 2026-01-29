3ee658faf2633bed52db056e8e970e8de4e9ceea
Vinaya-16 <vinayapatole16@gmail.com>
2026-01-30 01:02:20 +0530

    Update business_insights.sql

--- scripts/Exploratory Data Analysis/business_insights.sql
+++ scripts/Exploratory Data Analysis/business_insights.sql
@@ -104,4 +104,61 @@ FROM categories_sales
 ORDER BY perc_sales DESC
 
 
+-- SEGMENT PRODUCTS INTO COST RANGES 
+-- AND COUNT HOW MANY PRODUCTS FALL INTO EACH SEGMENT
+WITH product_segmenets AS
+(
+	SELECT
+	product_key,
+	product_name,
+	product_cost,
+	CASE 
+		WHEN product_cost < 100 THEN 'Below 100'
+		WHEN product_cost BETWEEN 100 AND 500 THEN '100-500'
+		WHEN product_cost BETWEEN 500 AND 1000 THEN '500-1000'
+		ELSE 'Above 1000'
+	END cost_range
+	FROM gold.dim_products
+)
+SELECT
+cost_range,
+COUNT(product_key) AS total_products
+FROM product_segmenets
+GROUP BY cost_range
+ORDER BY total_products DESC
+
+
+-- GROUP CUSTOMERS INTO THREE SEGMENTS BASED ON THEIR SPENDING BEHAVIOR:
+-- - VIP: AT LEAST 12 MONTHS OF HISTORY AND SPENDING MORE THAN $5000
+-- - REGULAR: AT LEAST 12 MONTHS OF HISTORY BUT SPENDING $5000 OR LESS
+-- - NEW: LIFESPAN LESS THAN 12 MONTHS
+-- AND FIND THE TOTAL NUMBER OF CUSTOMERS BY EACH GROUP
+WITH customers_segments AS
+(
+	SELECT
+		c.customer_key,
+		SUM(f.sales_amount) AS spendings,
+		MIN(f.order_date) AS first_order,
+		MAX(f.order_date) AS last_order,
+		DATEDIFF(MONTH, MIN(f.order_date), MAX(f.order_date)) AS months_connected,
+		CASE 
+			WHEN SUM(f.sales_amount) > 5000 AND DATEDIFF(MONTH, MIN(f.order_date), MAX(f.order_date)) >= 12
+				THEN 'VIP'
+			WHEN SUM(f.sales_amount) <= 5000 AND DATEDIFF(MONTH, MIN(f.order_date), MAX(f.order_date)) >= 12
+				THEN 'Regular'
+			ELSE 'New'
+		END Type_of_customer
+	FROM gold.fact_sales f 
+	LEFT JOIN gold.dim_customers c
+	ON f.customer_key = c.customer_key
+	GROUP BY c.customer_key
+)
+SELECT
+Type_of_customer,
+COUNT(customer_key) AS total_customers
+FROM customers_segments
+GROUP BY Type_of_customer
+
+
+
 
